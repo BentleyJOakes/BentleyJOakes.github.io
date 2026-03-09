ede5320abae4ea8f619e6f734ef8427fd4dc3bbe
BentleyJOakes <BentleyJOakes@users.noreply.github.com>
2026-03-09 02:17:53 +0000

    Deploying to gh-pages from @ BentleyJOakes/BentleyJOakes.github.io@515fae0cfd038b9e518509046af59a883c4a3210 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
     },
   },{id: "nav-publications",
-          title: "publications",
+          title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
