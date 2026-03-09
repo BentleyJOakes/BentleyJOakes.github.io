f489d934c14b4a4ca23f19621daf99d09edacb39
BentleyJOakes <BentleyJOakes@users.noreply.github.com>
2026-03-09 14:05:54 +0000

    Deploying to gh-pages from @ BentleyJOakes/BentleyJOakes.github.io@ca538d903666a66c310d564574e032dc0c95de5c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -139,7 +139,7 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/BOakes-CV.pdf", "_blank");
+          window.open("/assets/pdf/Bentley_Oakes_CV.pdf", "_blank");
         },
       },{
         id: 'social-email',
